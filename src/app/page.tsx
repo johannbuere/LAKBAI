82eea34dc2caf37f0f4477538baab702b91be233
johannbuere <johannbuere@gmail.com>
2025-09-17 11:50:25 +0800

    Clarified page structures for website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="text-center pt-12">
+      <h1 className="text-3xl capitalize font-bold mb-4">
+        LAKBAI
+      </h1>
+      <p className="text-[16px]">This is a Next.js website for our navigation app.</p>
+    </div>
+  );
 }
---
+++ src/app/plan/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div>This would have two sides. Left for planning, right for the map</div>
+  )
+}
